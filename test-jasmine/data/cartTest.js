0608e1490172caa4d55e5fcd606b3e836cf657ae
samuel <lakamosamuel7@gmail.com>
2025-05-15 21:48:21 +0100

    test the add to existing '

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -1,7 +1,27 @@
 import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe("test suit: add to cart", () => {
-    it("adds an existing product to the cart", () => {});
+    it("adds an existing product to the cart", () => {
+        spyOn(localStorage, "setItem");
+        spyOn(localStorage, "getItem").and.callFake(() => {
+            return JSON.stringify([
+                {
+                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                    quantity: 1,
+                    deliveryOptionId: "1"
+                }
+            ]);
+        });
+        loadFromStorage();
+
+        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual(
+            "e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
+        );
+        expect(cart[0].quantity).toEqual(2);
+    });
     it("add a new product", () => {
         spyOn(localStorage, "setItem");
         spyOn(localStorage, "getItem").and.callFake(() => {
@@ -15,6 +35,5 @@ describe("test suit: add to cart", () => {
         expect(cart[0].productId).toEqual(
             "e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
         );
-        
     });
 });
