d947cdc7e4ed26b8e814f8ef4bac2c957e1488c5
samuel <lakamosamuel7@gmail.com>
2025-05-26 14:41:48 +0100

    use promise.all func

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,7 +5,6 @@ import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 // import "../data/backend-pratice.js";
 import { loadProducts } from "../data/products.js";
 
-
 /*new Promise( resolve => {
   loadProducts(() => {
     resolve();
@@ -14,7 +13,31 @@ import { loadProducts } from "../data/products.js";
 loadProducts(() => {
     
 });*/
-loadProducts(() => {
-  renderOrderSummary();
+Promise.all([
+    new Promise(resolve => {
+        loadProducts(() => {
+            resolve("value1");
+        });
+    }),
+    new Promise(resolve => {
+        return setTimeout(() => {
+            console.log("hello");
+            resolve();
+        }, 5000);
+    })
+]).then(values => {
+    console.log(values);
+    renderOrderSummary();
     renderPaymentSummary();
-})
+});
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  })
+}).then(() => {
+  
+})*/
+/*loadProducts(() => {
+  
+})*/
